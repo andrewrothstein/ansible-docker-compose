536a98a62ac2ac92850010bb2f35e356ff983eb4
Andrew Rothstein <andrew.rothstein@gmail.com>
2017-07-27 12:00:20 -0400

    up to v1.15.0

---
+++ dl-checksum.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+VER=1.15.0
+DIR=~/Downloads
+MIRROR=https://github.com/docker/compose/releases/download
+APP=docker-compose
+for platform in Darwin-x86_64 Linux-x86_64 Windows-x86_64
+do
+    wget -O $DIR/$APP-$platform.$VER $MIRROR/$VER/$APP-$platform
+done
+sha256sum $DIR/$APP-*.$VER
+#shasum -a 256 $DIR/$APP-*.$VER
