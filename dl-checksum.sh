eaf954805f6310faf130d6a4db086b8b855678b6
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-10-31 16:24:42 -0500

    my sh is getting better

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env sh
-VER=1.24.1
+VER=${1:-1.24.1}
 DIR=~/Downloads
 MIRROR=https://github.com/docker/compose/releases/download/$VER
 
 dl()
 {
-    OS=$1
-    ARCH=$2
-    SUFFIX=${3:-}
-    SHAFILE=docker-compose-${OS}-${ARCH}${SUFFIX}.sha256
-    URL=$MIRROR/$SHAFILE
+    local os=$1
+    local arch=$2
+    local suffix=${3:-}
+    local shafile=docker-compose-${os}-${arch}${suffix}.sha256
+    local url=$MIRROR/$shafile
 
-    printf "    # %s\n" $URL
-    printf "    %s-%s: sha256:%s\n" $OS $ARCH `curl -sSL $URL | awk '{print $1}'`
+    printf "    # %s\n" $url
+    printf "    %s-%s: sha256:%s\n" $os $arch `curl -sSL $url | awk '{print $1}'`
 }
 
 printf "  '%s':\n" $VER
