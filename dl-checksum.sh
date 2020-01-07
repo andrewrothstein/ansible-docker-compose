f4ae66625eb6071466ad63738747a0df08241e8d
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-01-07 09:18:29 -0500

    up to v1.25.1

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env sh
-VER=${1:-1.25.0}
 DIR=~/Downloads
-MIRROR=https://github.com/docker/compose/releases/download/$VER
+MIRROR=https://github.com/docker/compose/releases/download
 
 dl()
 {
-    local os=$1
-    local arch=$2
-    local suffix=${3:-}
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local suffix=${4:-}
     local shafile=docker-compose-${os}-${arch}${suffix}.sha256
-    local url=$MIRROR/$shafile
+    local url=$MIRROR/$ver/$shafile
 
     printf "    # %s\n" $url
-    printf "    %s-%s: sha256:%s\n" $os $arch `curl -sSL $url | awk '{print $1}'`
+    printf "    %s-%s: sha256:%s\n" $os $arch $(curl -sSL $url | awk '{print $1}')
 }
 
-printf "  '%s':\n" $VER
-dl Darwin x86_64
-dl Linux x86_64
-dl Windows x86_64 .exe
+dl_ver() {
+    ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver Darwin x86_64
+    dl $ver Linux x86_64
+    dl $ver Windows x86_64 .exe
+}
 
 
+dl_ver ${1:-1.25.1}
