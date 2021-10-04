82cb42180d32570b428627361139f9665f38d4eb
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-10-04 19:29:03 -0400

    compose:2.0.1 as a docker cli plugin

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -2,6 +2,8 @@
 DIR=~/Downloads
 MIRROR=https://github.com/docker/compose/releases/download
 
+# https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-linux-x86_64.sha256
+
 dl()
 {
     local ver=$1
@@ -9,7 +11,7 @@ dl()
     local arch=$3
     local suffix=${4:-}
     local shafile=docker-compose-${os}-${arch}${suffix}.sha256
-    local url=$MIRROR/$ver/$shafile
+    local url=$MIRROR/v$ver/$shafile
 
     printf "    # %s\n" $url
     printf "    %s-%s: sha256:%s\n" $os $arch $(curl -sSL $url | awk '{print $1}')
@@ -18,10 +20,9 @@ dl()
 dl_ver() {
     ver=$1
     printf "  '%s':\n" $ver
-    dl $ver Darwin x86_64
-    dl $ver Linux x86_64
-    dl $ver Windows x86_64 .exe
+    dl $ver darwin x86_64
+    dl $ver linux x86_64
+    dl $ver windows x86_64 .exe
 }
 
-
-dl_ver ${1:-1.29.2}
+dl_ver ${1:-2.0.1}
