e39f25bcdce7f2a58c07f2a9497cd4c48d775b45
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-08-20 23:31:09 -0400

    compose:2.10.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,28 +1,46 @@
 #!/usr/bin/env sh
+set -e
 DIR=~/Downloads
 MIRROR=https://github.com/docker/compose/releases/download
 
-# https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-linux-x86_64.sha256
+# https://github.com/docker/compose/releases/download/v2.10.0/checksums.txt
 
 dl()
 {
-    local ver=$1
-    local os=$2
-    local arch=$3
-    local suffix=${4:-}
-    local shafile=docker-compose-${os}-${arch}${suffix}.sha256
-    local url=$MIRROR/v$ver/$shafile
+    local lchecksums=$1
+    local ver=$2
+    local os=$3
+    local arch=$4
+    local dotexe=${5:-}
+    local platform="${os}-${arch}"
+    local exe=docker-compose-${platform}${dotexe}
+    # https://github.com/docker/compose/releases/download/v2.10.0/docker-compose-linux-x86_64
+    local url=$MIRROR/v$ver/$exe
 
     printf "    # %s\n" $url
-    printf "    %s-%s: sha256:%s\n" $os $arch $(curl -sSLf $url | awk '{print $1}')
+    printf "    %s: sha256:%s\n" $platform $(fgrep $exe $lchecksums | awk '{print $1}')
 }
 
 dl_ver() {
     ver=$1
     printf "  '%s':\n" $ver
-    dl $ver darwin x86_64
-    dl $ver linux x86_64
-    dl $ver windows x86_64 .exe
+    local lchecksums="${DIR}/docker-compose-${ver}-checksums.txt"
+    if [ ! -e $lchecksums ];
+    then
+        curl -sSLf -o $lchecksums "${MIRROR}/v${ver}/checksums.txt"
+    fi
+
+    dl $lchecksums $ver darwin aarch64
+    dl $lchecksums $ver darwin x86_64
+    dl $lchecksums $ver linux aarch64
+    dl $lchecksums $ver linux armv6
+    dl $lchecksums $ver linux armv7
+    dl $lchecksums $ver linux ppc64le
+    dl $lchecksums $ver linux riscv64
+    dl $lchecksums $ver linux s390x
+    dl $lchecksums $ver linux x86_64
+    dl $lchecksums $ver windows aarch64 .exe
+    dl $lchecksums $ver windows x86_64 .exe
 }
 
-dl_ver ${1:-2.9.0}
+dl_ver ${1:-2.10.0}
